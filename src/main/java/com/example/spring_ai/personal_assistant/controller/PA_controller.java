99704da3d66627d40bcee8eb3d5c5fb4c2c3559b
learning_git <shivamittal616@gmail.com>
2026-05-02 01:42:37 +0530

    endpoint /ask take user and systemPromtTemplate

--- src/main/java/com/example/spring_ai/personal_assistant/controller/PA_controller.java
+++ src/main/java/com/example/spring_ai/personal_assistant/controller/PA_controller.java
@@ -1,9 +1,16 @@
 package com.example.spring_ai.personal_assistant.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ai")
@@ -14,8 +21,23 @@ public class PA_controller {
         this.chatClient = chatClient;
     }
 
-        @GetMapping("/chat")
-        public String chat(@RequestParam String question) {
+    @GetMapping("/chat")
+    public String chat(@RequestParam String question){
         return chatClient.prompt().user(question).call().content();
-        }
+    }
+    @GetMapping("/ask")
+    public String ask(@RequestParam String question  ,  @RequestParam String persona) {
+        String blueprint = " play a persona of {persona}";
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(blueprint);
+
+        Message sysmessage = systemPromptTemplate.createMessage(Map.of("persona",persona));
+        Message usermessage = new UserMessage(question);
+
+        Prompt prompt = new Prompt(sysmessage,usermessage);
+
+        return chatClient.prompt(prompt).call().content();
+
+
+
+    }
 }
