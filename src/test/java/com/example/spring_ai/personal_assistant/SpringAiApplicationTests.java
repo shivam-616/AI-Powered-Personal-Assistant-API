23abc3bad31de7a227a19ee4dc8abddc5c5e797b
learning_git <shivamittal616@gmail.com>
2026-05-01 16:47:14 +0530

    first commit - project structure

---
+++ src/test/java/com/example/spring_ai/personal_assistant/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai.personal_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
